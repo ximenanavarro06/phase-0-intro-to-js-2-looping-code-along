50d0b40cb83cbfb3bed541977dfdf2e18980c135
ximenanavarro <ximenanavarro@Ximenas-MacBook-Pro.local>
2022-11-07 10:13:11 -0700

    commit tree 40ddf0cc6f6c13e1c30efa2d703ab5ad22a9ce64

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+        console.log(thankYouCards(`Thank you, ${names[i]} for the wonderful ${event} gift!`))
+    }
+    return thankYouCards
+}
